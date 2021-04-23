70c51eb788cf52d9a16a9dc614d49f6823cfd659
Bayu Kusuma <bayukusuma.bk@gmail.com>
2021-04-23 23:29:13 +0800

    Operator Binary Aritmatika

--- src/operator/OperatorBinary.java
+++ src/operator/OperatorBinary.java
@@ -4,4 +4,13 @@ public class OperatorBinary {
     String nama = "Muhammad Bayu Kusuma Adjie";
     int jumlahSks = 24;
     double indeksOrestasuKumulatif = 3.89;
+
+    public static void main(String[] args) {
+        System.out.println(9+3);
+        System.out.println(9-3);
+        System.out.println(9*3);
+        System.out.println(9/3);
+        System.out.println(9%3);
+        System.out.println(9%2);
+    }
 }
